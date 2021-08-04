d073af96d1746486d43d1732498200e26b1acb71
Ran-n <ran-n@tutanota.com>
2021-08-05 00:03:24 +0200

    excutar windows

---
+++ flappy_bird.bat
@@ -0,0 +1,1 @@
+python flappy_bird.py
--- flappy_bird.py
+++ flappy_bird.py
@@ -11,8 +11,8 @@ pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('carlistas.png').convert()
-#bg_surface = pygame.transform.scale2x(bg_surface)
+bg_surface = pygame.image.load('media/imaxes/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
 
 while True:
 	for event in pygame.event.get():
